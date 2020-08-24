63dfc2e8f647e9dd9143dc9c185ef540be3fab9b
fjshomeplate <38842004+fjshomeplate@users.noreply.github.com>
2020-08-24 13:26:13 -0400

    Content+Bug Swatting
    Content:
    Add Content to MainContent
    
    Bug Swatting:
    Fix a bug that prevented Compiling

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,31 +38,15 @@ const Bio = () => {
   const { author, social } = data.site.siteMetadata
   return (
     <div
-      style={{
-        display: `flex`,
-        marginBottom: rhythm(2.5),
-      }}
-    >
-      <Image
-        fixed={data.avatar.childImageSharp.fixed}
-        alt={author.name}
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      />
-      <p>
-        Written by <strong>{author.name}</strong> {author.summary}
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
-      </p>
+    <h1>
+    Go Lakey
+    </h1>
+    <h2>
+  Some History
+    </h2>
+    <p>
+    The Lakeland Terrier, a member of the Terrier Family, is a smart, playful, meduim-high energy dog breed. 
+    </p>
     </div>
   )
 }
