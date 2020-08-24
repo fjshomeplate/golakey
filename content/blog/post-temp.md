60e47ff8ec1b60adce165008eff294cf766abf55
fjshomeplate <38842004+fjshomeplate@users.noreply.github.com>
2020-08-24 13:39:10 -0400

    4/10+

--- content/blog/post-temp.md
+++ content/blog/post-temp.md
@@ -1,3 +1,7 @@
-Welcome aboard GoLakey.com!!! Please follow the commit guide, outlined below.
-Bug Swatting - Bugs
-Content - Content
+----
+title: Go Lakey
+date: "2015-05-01T22:12:03.284Z"
+description: "Hello World"
+---
+
+Welcome to GoLakey.com - this website is written in GatsbyJS. DO NOT REMOVE THIS POST UNTIL CONTENT ROLLS IN!!!!!!!!!!
--- src/components/bio.js
+++ src/components/bio.js
@@ -47,6 +47,12 @@ const Bio = () => {
     <p>
     The Lakeland Terrier, also known as the Patterdale Terrier, a member of the Terrier Family. They were used to hunt foxes and otters, in Lake District, England, which is how they got their names.
     </p>
+    <h2>
+    Coat
+    </h2>
+    <p>
+    The Lakeland Terrier's coat, as a puppy, is soft. However, around two months, wire starts to come in. The soft coat should then be plucked out. If you do not like the wirey coat, you should not pluck it, and instead use clippers. If you do clipper it, the wire will go away forever.
+    </p>
     </div>
   )
 }
